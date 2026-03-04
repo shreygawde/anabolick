efe8153be076625fd4cf4837735185260a5d2f90
Barhammer <kaizadprinter@gmail.com>
2026-03-04 22:29:00 +0530

    Fix project structure: move frontend into client folder

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    
-         <footer className="p-6 text-center text-gray-500 text-sm border-t border-gray-800">
-        © {new Date().getFullYear()} ANABOLICK — Built by Shrey Gawde
-      </footer>
-      
-  );
-  }
