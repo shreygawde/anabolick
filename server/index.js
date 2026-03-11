2e647d7bc1e8210370b45646bb0ec3bfe98de826
Barhammer <kaizadprinter@gmail.com>
2026-03-11 23:49:26 +0530

    integrated nginx

--- server/index.js
+++ server/index.js
@@ -7,11 +7,19 @@ const analyzeRoute = require("./routes/analyzeRoute");
 
 const app = express();
 
+// important when running behind nginx
+app.set("trust proxy", true);
+
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Anabolick API running");
+});
 app.use("/analyze-text", analyzeRoute);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+// bind to all interfaces
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
