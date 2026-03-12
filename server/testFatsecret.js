f3997596888711ef6e7a02f4b7b70b41a29fd8dc
Barhammer <kaizadprinter@gmail.com>
2026-03-12 06:19:43 +0530

    testing vps

--- server/testFatsecret.js
+++ server/testFatsecret.js
@@ -1,7 +1,28 @@
 require("dotenv").config();
+const axios = require("axios");
 const { getAccessToken } = require("./services/fatsecretService");
 
-(async () => {
+async function searchFood(query) {
   const token = await getAccessToken();
-  console.log("Token:", token);
+
+  const response = await axios.get(
+    "https://platform.fatsecret.com/rest/server.api",
+    {
+      params: {
+        method: "foods.search",
+        search_expression: query,
+        format: "json"
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+
+  return response.data;
+}
+
+(async () => {
+  const result = await searchFood("egg");
+  console.log(JSON.stringify(result, null, 2));
 })();
