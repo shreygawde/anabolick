58fbeef734e6a36bd4e03cf20181b9319f333453
Barhammer <kaizadprinter@gmail.com>
2026-03-12 06:54:42 +0530

    test

--- server/services/fatsecretService.js
+++ server/services/fatsecretService.js
@@ -1,5 +1,26 @@
 const axios = require("axios");
-const { getAccessToken } = require("./fatsecretService"); // if same file adjust later
+
+async function getAccessToken() {
+
+  const response = await axios.post(
+    "https://oauth.fatsecret.com/connect/token",
+    new URLSearchParams({
+      grant_type: "client_credentials",
+      scope: "basic"
+    }),
+    {
+      auth: {
+        username: process.env.FATSECRET_CLIENT_ID,
+        password: process.env.FATSECRET_CLIENT_SECRET
+      },
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+  );
+
+  return response.data.access_token;
+}
 
 async function searchFood(query) {
 
