8c553e77b6c28a4ba1323e462654ffde757643f1
Barhammer <kaizadprinter@gmail.com>
2026-03-14 00:57:35 +0530

    test

--- server/utils/nutritionMath.js
+++ server/utils/nutritionMath.js
@@ -23,3 +23,25 @@ function normalizeNutrition(nutrition, amount, unit) {
     fat: fatPerUnit * amount
   };
 }
+function calculateTotals(ingredients) {
+
+  return ingredients.reduce(
+    (totals, item) => {
+      totals.calories += item.calories || 0;
+      totals.protein += item.protein || 0;
+      totals.carbs += item.carbs || 0;
+      totals.fat += item.fat || 0;
+      return totals;
+    },
+    {
+      calories: 0,
+      protein: 0,
+      carbs: 0,
+      fat: 0
+    }
+  );
+}
+module.exports = {
+  normalizeNutrition,
+  calculateTotals
+};
