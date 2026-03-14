974e77e420ed010005980806f784611bafd3a4d5
Barhammer <kaizadprinter@gmail.com>
2026-03-14 20:22:16 +0530

    test

--- client/src/pages/MacroTracker.jsx
+++ client/src/pages/MacroTracker.jsx
@@ -93,7 +93,7 @@ const proteinProgress = proteinTarget
   try {
     setAiLoading(true);
 
-    const response = await fetch("/api/analyze-text", {
+    const response = await fetch("/analyze-text", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
