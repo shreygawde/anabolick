a1de7c4dce14f596d3850cce7dee2e7f57ab1b2a
Barhammer <kaizadprinter@gmail.com>
2026-03-22 16:56:23 +0530

    r

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const analyzeRoute = require("./routes/analyzeRoute");
+/*const analyzeRoute = require("./routes/analyzeRoute");
 const summaryRoute = require("./routes/summaryRoute");
 const weeklyRoute = require("./routes/weeklyRoute");
 const userRoute = require("./routes/userRoute");
-
+*/
 const app = express();
 
 app.use(cors());
@@ -17,11 +17,11 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.use("/analyze-text", analyzeRoute);
+/*app.use("/analyze-text", analyzeRoute);
 app.use("/summary", summaryRoute);
 app.use("/weekly", weeklyRoute);
 app.use("/user", userRoute);
-
+*/
 const PORT = 5000;
 
 app.listen(PORT, () => {
